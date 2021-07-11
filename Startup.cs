98702a16a1d339b2f782462c62a49fb527898d99
Shubham N J <shubhamsnj@gmail.com>
2021-07-11 11:50:39 +0530

    added ef tool

--- Startup.cs
+++ Startup.cs
@@ -88,6 +88,8 @@ namespace Hotel
             });
         }
 
+
+
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
